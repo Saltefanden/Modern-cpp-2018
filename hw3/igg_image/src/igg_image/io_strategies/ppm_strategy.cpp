e64df4c5b5ab3e84bf1d17c62e24f15ef761f54d
Nicklas NNIT <nimu@nnit.com>
2022-04-13 21:24:04 +0000

    Adds PpmIoStrategy files with barebones content, adds references in io_strategies/CMakeLists.txt.

---
+++ hw3/igg_image/src/igg_image/io_strategies/ppm_strategy.cpp
@@ -0,0 +1,15 @@
+#include<ppm_strategy.h>
+#include<string>
+#include<vector>
+
+namespace igg {
+  bool PpmIoStrategy::Write(const std::string& file_name, const ImageData& data) const {
+    
+  }
+  
+  ImageData PpmIoStrategy::Read(const std::string& file_name) const {
+
+  }
+
+
+} // namespace igg.
---
+++ hw3/igg_image/src/igg_image/io_strategies/ppm_strategy.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <png++/png.hpp>
+#include<strategy.h>
+
+namespace igg {
+
+class PpmIoStrategy : public IoStrategy {
+ public:
+  bool Write(const std::string& file_name,
+             const ImageData& data) const override;
+  ImageData Read(const std::string& file_name) const override;
+};
+
+}  // namespace igg
