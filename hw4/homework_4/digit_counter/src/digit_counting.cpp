c4f32d7773d6a96b429ad30dace4acd61c9d3d14
Nicklas@privateComputer <Saltefanden@github.com>
2022-04-19 10:10:33 +0200

    Sets up and begins homework4

---
+++ hw4/homework_4/digit_counter/src/digit_counting.cpp
@@ -0,0 +1,14 @@
+#include<digit_counting.h>
+
+bool ReturnTrue(){
+  return true;
+}
+
+double Warp(double number, double factor){
+  double temp = number + factor;
+  return temp - factor;
+}
+
+int CountSameSignificantDigits(double a, double b){
+  
+}
